560c7642ef95931d9d716b468ddae72f23d3c63b
B. W. Lewis <blewis@illposed.net>
2014-12-05 18:34:38 -0500

    Removed dependencies on png, jpeg packages

--- inst/examples/hello/server.R
+++ inst/examples/hello/server.R
@@ -10,7 +10,7 @@ shinyServer(function(input, output) {
     b <- gsub(" ","0",sprintf("%2x",floor(2.55*input$diffuse_b)))
     diffuse <- sprintf("#%s%s%s",r,g,b)
     specular <- sprintf("#%s",paste(rep(gsub(" ","0", sprintf("%2x",floor(2.55*input$specular))),3),collapse=""))
-    helloThree.js(img=readPNG(system.file("img/Rlogo.png",package="png")),
+    helloThree.js(img=texture(system.file("img/Rlogo.jpg",package="threejs")),
                   ambient=diffuse,specular=specular, shininess=input$shininess)
   })
   
