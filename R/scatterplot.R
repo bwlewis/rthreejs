3825958f3356727399cafd4936b1b57d41dcb03c
B. W. Lewis <blewis@illposed.net>
2014-12-29 13:50:03 -0500

    Fixed typo in scatterplot3js doc

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -60,7 +60,7 @@
 #' z <- seq(-10, 10, 0.01)
 #' x <- cos(z)
 #' y <- sin(z)
-#' scatterplot3js(x,y,z, rainbow(length(z)))
+#' scatterplot3js(x,y,z, color=rainbow(length(z)))
 #'
 #' # A shiny example
 #' library("shiny")
