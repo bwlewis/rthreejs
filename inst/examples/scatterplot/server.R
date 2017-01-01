dd4410c0510c6de041896e0d634f1880fd28611d
bwlewis <blewis@illposed.net>
2016-12-31 23:04:47 -0500

    basic shiny example update

--- inst/examples/scatterplot/server.R
+++ inst/examples/scatterplot/server.R
@@ -9,21 +9,16 @@ shinyServer(function(input, output)
   output$scatterplot <- renderScatterplotThree({
     num.ticks <- input$nticks
     if (num.ticks == 0) num.ticks <- NULL
-    else num.ticks <- rep(num.ticks,3)
-    if (input$radius) r <- rlnorm(50,meanlog = -4, sdlog = 0.5)
-    else r <- 0
-    data <- cbind(example_data,r)
+    else num.ticks <- rep(num.ticks, 3)
+    data <- example_data
     colnames(data) <- NULL
     color <- rep(rainbow(input$colors),length.out = nrow(data))
     sizes <- rep(c(0.5, 1, 2)[1:input$sizes], length.out = nrow(data))
-    labs <- sprintf("x=%.2f, y=%.2f, z=%.2f r=%.2f", data[,1], data[,2], data[,3], data[,4])
     scatterplot3js(x = data,
                     num.ticks = num.ticks,
                     color = color,
                     size = sizes,
-                    labels = labs,
                     label.margin = "80px 10px 10px 10px",
-                    renderer = input$renderer,
                     grid = input$grid)
   })
 })
--- inst/examples/scatterplot/ui.R
+++ inst/examples/scatterplot/ui.R
@@ -9,13 +9,9 @@ shinyUI(fluidPage(
     sidebarPanel(
       numericInput("nticks", "Number of ticks", 6, min = 0, max = 10, step = 1),
       numericInput("colors", "Number of colors", 5, min = 1, max = 8, step = 1),
-      numericInput("sizes", "Number of sizes (only affects canvas renderings)", 2, min = 1, max = 3, step = 1),
-      selectInput("renderer", label="Rendering method",
-         choices = list("Auto"="auto", "Canvas"="canvas", "WebGL"="webgl"),
-         selected = 1),
+      numericInput("sizes", "Number of sizes", 2, min = 1, max = 3, step = 1),
       checkboxInput("grid", label = "Grid", value = TRUE),
-      checkboxInput("radius", label = "Radius", value = FALSE),
-      p("Use the mouse zoom to zoom in/out.")
+      p("Use the mouse zoom to rotate, zoom, and pan.")
     ),
     mainPanel(
         scatterplotThreeOutput("scatterplot")
