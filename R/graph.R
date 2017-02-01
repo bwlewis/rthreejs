566e2acc53dc8f8b014d2eb270452400cc52c2d2
bwlewis <blewis@illposed.net>
2017-02-01 09:16:18 -0500

    doc

--- R/graph.R
+++ R/graph.R
@@ -61,14 +61,14 @@
 #' are clicked interactively, where \code{list} is a named list of animation entries.
 #' Each entry must itself be a list with the following entries
 #' \itemize{
-#' \item{}{ optional igraph object with the same number of vertices as \code{g} above (if specified this must be the first entry)}
-#' \item{layout}{ - optional igraph layout}
-#' \item{vertex.color}{ - optional vector of vertex colors}
-#' \item{edge.color}{ - optional vector of edge colors}
+#' \item{}{ optional a single igraph object with the same number of vertices as \code{g} above (if specified this must be the first entry)}
+#' \item{layout}{ - optional a single igraph layout, or differential layout if \code{cumulative=TRUE}}
+#' \item{vertex.color}{ - optional single vector of vertex colors}
+#' \item{edge.color}{ - optional single vector of edge colors}
 #' \item{cumulative}{ - optional boolean entry, if \code{TRUE} then vertex positions are added to current plot, default is \code{FALSE}}
 #' }
 #' At least one of \code{g} or \code{layout} must be specified in each animation list entry.
-#' The layouts and colors can be optionally imbedded in the igraph object as described above.
+#' The layouts and colors may be alternatively imbedded in the igraph object itself.
 #' Each animation list entry must be named by a number corresponding to the vertex
 #' enumeration in \code{g}. An animation sequence is triggered when a corresponding
 #' vertex is clicked. For instance, to trigger animations when vertices number 1 or 5 are
